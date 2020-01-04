0001e88fa79255de668aa7e5bd56c3a9c141c1dc
Kennedy Mwenja <mwenja07@gmail.com>
2020-01-04 13:22:37 +0300

    Apply scrollback patch
    - modify keys used by patch, now uses Alt+j and Alt+k to scroll
    - also change copy and paste keys to Ctrl+Alt+c/v

--- config.def.h
+++ config.def.h
@@ -179,7 +179,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -190,11 +190,13 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ (ControlMask|MODKEY), XK_c,           clipcopy,       {.i =  0} },
+	{ (ControlMask|MODKEY), XK_v,           clippaste,      {.i =  0} },
+	{ (ControlMask|MODKEY), XK_p,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,               XK_k,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_j,           kscrolldown,    {.i = -1} },
 };
 
 /*
