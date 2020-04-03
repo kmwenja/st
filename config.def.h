13f19c023cc9401fcd6da564c6fddeefe9a7c586
Kennedy Mwenja <mwenja07@gmail.com>
2020-04-03 18:42:35 +0300

    Fix default cursor color designation
    - defaultcs is now foreground and defaultrcs is background
    - make `config.h` a symbolic link to `config.def.h`
    - add a .gitignore file to ignore build artifacts

--- config.def.h
+++ config.def.h
@@ -125,8 +125,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
